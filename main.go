f84f5e3151dff2e3c0900f1049871a46557a3abf
Michael Krill <michael.krill@telekom.de>
2020-05-28 13:42:39 +0200

    LiveCoding 28.05.2020

--- main.go
+++ main.go
@@ -1,15 +1,35 @@
 package main
 
 import (
+	"errors"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/mkrill/gosea/status"
 )
 
 func main() {
+	var err error
 
-	mux := http.NewServeMux() // similar to constructor, make() with standard data types
+	logfile, err := os.Create("messages.log")
+	if err != nil {
+		log.Fatal("error opening log file")
+	}
+	defer func() {
+		log.Print("closing logfile")
+		logfile.Close()
+	}()
 
+	logger := log.New(os.Stdout, "gosea ", log.LstdFlags)
+
+	sigChan := make(chan os.Signal)
+	defer close(sigChan)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM) // channels gets signal, if application is terminated
+
+	mux := http.NewServeMux()                // similar to constructor, make() with standard data types
 	mux.HandleFunc("/health", status.Health) // function should not be called, therefore no parameter
 
 	srv := &http.Server{
@@ -17,5 +37,18 @@ func main() {
 		Handler: mux,
 	} // For clarity directly as a pointer
 
-	srv.ListenAndServe()
+	go func() {
+		err := srv.ListenAndServe() // function is blocking until server is terminated
+		if err != nil && !errors.Is(err, http.ErrServerClosed) {
+			logger.Fatalf("error starting server: %s", err.Error())
+		}
+	}()
+
+	logger.Print("Started service")
+
+	<-sigChan // code line blocks, until signal is received from channel
+
+	srv.Close()
+
+	logger.Print("stopping service")
 }
