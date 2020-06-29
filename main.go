4e0923333d213d34fd0fb36ba16c7b9f637739bb
Peter Teich <peter.teich@motor.de>
2020-06-29 14:56:33 +0200

    Add healtheck module

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flamingo.me/dingo"
 	"flamingo.me/flamingo/v3"
+	"flamingo.me/flamingo/v3/core/healthcheck"
 
 	"github.com/mkrill/gosea/src/seabackend"
 )
@@ -10,6 +11,7 @@ import (
 func main() {
 
 	flamingo.App([]dingo.Module{
+		new(healthcheck.Module),
 		new(seabackend.Module),
 	})
 }
